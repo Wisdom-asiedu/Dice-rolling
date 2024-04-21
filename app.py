4188f98698df6a44539304ade6ac158a7f75bdfa
Mr. Ryt <flaviosobbin04@gmail.com>
2024-04-22 00:43:50 +0100

    pyglet finit

--- app.py
+++ app.py
@@ -1,68 +1,95 @@
 import pyglet
 import random
 
-# Initialize the window
-window = pyglet.window.Window(800, 600, "Dice Rolling Simulator")
+class DiceSimulator(pyglet.window.Window):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self.dice_size = 50
+        self.dice_spacing = 20
+        self.previous_rolls = []
+        self.roll_again = False
+        
+        self.num_dice = 0
+        self.num_faces = 0
 
-# Set up the background color
-pyglet.gl.glClearColor(0.2, 0.3, 0.2, 0.8)
+        # Create text inputs
+        self.dice_input_label = pyglet.text.Label('Number of Dice:', font_size=14, x=20, y=50)
+        self.dice_input = pyglet.text.Label('', font_size=14, x=150, y=50)
+        
+        self.faces_input_label = pyglet.text.Label('Number of Faces:', font_size=14, x=20, y=20)
+        self.faces_input = pyglet.text.Label('', font_size=14, x=150, y=20)
 
-# Define some constants
-DICE_SIZE = 50
-DICE_SPACING = 20
-PREVIOUS_ROLLS_X = 20
-PREVIOUS_ROLLS_Y = 500
+        # Create buttons
+        self.roll_button = pyglet.text.Label('Roll', font_size=14, x=300, y=20)
+        self.roll_again_button = pyglet.text.Label('Roll Again', font_size=14, x=400, y=20)
 
-# Define global variables
-num_dice = int(input("dice: "))
-num_faces = int(input("faces: "))
-previous_rolls = []
+        # Set up text input handlers
+        self.dice_input.caret = "|"
+        self.faces_input.caret = "|"
 
-# Define a dice label
-dice_label = pyglet.text.Label("Number of Dice: {}   Number of Faces: {}".format(num_dice, num_faces),
-                               font_name='Times New Roman',
-                               font_size=16,
-                               x=20, y=570,
-                               anchor_x='left', anchor_y='top')
+        # Initialize number of dice and faces
+        self.num_dice = 0
+        self.num_faces = 0
 
-# Define a function to draw a single dice
-def draw_dice(x, y, value):
-    dice_sprite = pyglet.image.load('src/assets/dice{}.png'.format(value))
-    dice_sprite.blit(x, y, width=DICE_SIZE, height=DICE_SIZE)
+    def draw_dice(self, x, y, value):
+        dice_sprite = pyglet.image.load('src/assets/dice{}.png'.format(value))
+        dice_sprite.blit(x, y, width=self.dice_size, height=self.dice_size)
 
-# Define a function to draw the previous rolls
-def draw_previous_rolls():
-    y = PREVIOUS_ROLLS_Y
-    for roll in previous_rolls:
-        roll_label = pyglet.text.Label(str(roll), font_name='Times New Roman', font_size=12, x=PREVIOUS_ROLLS_X, y=y, anchor_x='left', anchor_y='top')
-        roll_label.draw()
-        y -= 20
+    def draw_previous_rolls(self):
+        y = 200
+        for roll in self.previous_rolls:
+            roll_label = pyglet.text.Label(str(roll), font_size=14, x=20, y=y)
+            roll_label.draw()
+            y -= 20
 
-# Define the on_draw event handler
-@window.event
-def on_draw():
-    window.clear()
-    dice_label.draw()
-    x = 20
-    y = 20
-    roll = []
-    for i in range(num_dice):
-        value = random.randint(1, num_faces)
-        draw_dice(x, y, value)
-        roll.append(value)
-        x += DICE_SIZE + DICE_SPACING
-    previous_rolls.append(roll)
-    draw_previous_rolls()
+    def on_draw(self):
+        self.clear()
+        self.dice_input_label.draw()
+        self.dice_input.draw()
+        self.faces_input_label.draw()
+        self.faces_input.draw()
+        self.roll_button.draw()
+        self.roll_again_button.draw()
+        
+        x = 20
+        y = 150
+        if self.num_dice > 0 and self.num_faces > 0:
+            for i in range(self.num_dice):
+                value = random.randint(1, self.num_faces)
+                self.draw_dice(x, y, value)
+                x += self.dice_size + self.dice_spacing
 
-# Define the on_key_press event handler
-@window.event
-def on_key_press(symbol, modifiers):
-    global num_dice, num_faces
-    if symbol == pyglet.window.key.N:
-        num_dice = int(pyglet.window.get_input().text(caption="Enter the number of dice:", initial="2"))
-    elif symbol == pyglet.window.key.F:
-        num_faces = int(pyglet.window.get_input().text(caption="Enter the number of faces:", initial="6"))
-    dice_label.text = "Number of Dice: {}   Number of Faces: {}".format(num_dice, num_faces)
+        self.draw_previous_rolls()
 
-# Start the Pyglet event loop
-pyglet.app.run()
+    def on_mouse_press(self, x, y, button, modifiers):
+        if button == pyglet.window.mouse.LEFT:
+            if 300 < x < 400 and 20 < y < 40:  # Roll Button
+                self.roll_dice()
+
+    def on_text(self, text):
+        if self.dice_input.text and self.faces_input.text:
+            self.num_dice = int(self.dice_input.text)
+            self.num_faces = int(self.faces_input.text)
+
+    def on_key_press(self, symbol, modifiers):
+        if symbol == pyglet.window.key.BACKSPACE:
+            self.dice_input.text = self.dice_input.text[:-1]
+            self.faces_input.text = self.faces_input.text[:-1]
+
+    def roll_dice(self):
+        if self.num_dice > 0 and self.num_faces > 0:
+            self.previous_rolls.clear()
+            for _ in range(self.num_dice):
+                roll = random.randint(1, self.num_faces)
+                self.previous_rolls.append(roll)
+
+    def update(self, dt):
+        if self.roll_again:
+            self.roll_dice()
+            self.roll_again = False
+
+if __name__ == "__main__":
+    window = DiceSimulator(800, 600, "Dice Rolling Simulator")
+    pyglet.clock.schedule_interval(window.update, 1/60)
+    pyglet.app.run()
