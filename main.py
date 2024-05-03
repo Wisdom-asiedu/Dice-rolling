6e2d9c060b530f782889bad32c6c492b9508105e
Mr. Ryt <125740894+MrRyt247@users.noreply.github.com>
2024-05-03 15:42:23 +0100

    Added dice art

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import random
+from mod.dice_art import dice_art as dice
 
 def roll_dice(num_dice: int, num_sides: int) -> None:
     rolls = []
-    total = 0
     for _ in range(num_dice):
         roll = random.randint(1, num_sides)
         rolls.append(roll)
-        total += roll
+        total = sum(rolls)
+
+    # Displays dice art for 9 sided-die. The number of dice depends on the width of your screen to prevent
+    # distortion of the art. It could be adjusted accordingly to fill the width of the terminam
+    if num_dice <= 12 and num_sides <= 9:               # Adjust num-dice to fill the width of your terminal
+        for line in range(5):
+            for roll in rolls:
+                print(dice.get(roll)[line], end="")
+            print()
+    else:
+        pass
+    # Outputs rresult
     print("Roll results:", rolls)
     print("Total score:", total)
 
@@ -17,7 +28,7 @@ def validate_input() -> None:
         if num_sides > 0:
             roll_dice(num_dice, num_sides)
         else:
-            print("The number on a die should be countable")
+            print("Cannot roll a die without sides")
     else:
         print("The number of dice should be countable")
 
@@ -37,9 +48,9 @@ def main():
                     quit()
                 else:
                     pass
-                
         except ValueError:
             print("Enter a counting number!\n")
 
+# Calls the main function
 if __name__ == "__main__":
     main()
